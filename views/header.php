325bb5525d7f06481c8e099aa512db37dc929367
audunb <audun@hbru.no>
2014-10-29 14:19:08 +0100

    Fixed path to css

--- views/header.php
+++ views/header.php
@@ -8,6 +8,9 @@ echo <<<_END
 Silicon Valley Startup
 </title>
 <link rel="stylesheet" type="text/css" href="$css">
+
+<link href='http://fonts.googleapis.com/css?family=Glegoo' rel='stylesheet' type='text/css'>
+
 <meta charset="UTF-8" />
 </head>
 <body>
