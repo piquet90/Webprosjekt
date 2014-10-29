fa53da443c88baa35c5a8b3c0c007381967926e6
audunb <audun@hbru.no>
2014-10-29 20:49:08 +0100

    css

--- views/index.php
+++ views/index.php
@@ -9,17 +9,30 @@ echo <<<_END
 
 
 
+
+
+
+
+<div id="frame">
+
 <h2>LOGG INN</h2>
+
 <form method="post" action="login.php">
 <input type="text" name="username" placeholder="Brukernavn" value="$user">
 <input type="password" name="password" placeholder="Passord">
 <input type="submit" value="Log in">
 <div id="error">$error</div>
 </form>
-<ul>
-	<li><a href="$registerpath">Bli medlem</a></li>
-	<li><a href="$forgotpath">Glemt passord</a></li>
+
+	<p><a href="$registerpath">Bli medlem</a
+	> | <a href="$forgotpath">Glemt passord</a
+	> | <a href="">FAQ</a
+	> | <a href="">Kontakt oss</a></p>
+	
 </ul>
+
+</div>
+
 
 </div>
 
