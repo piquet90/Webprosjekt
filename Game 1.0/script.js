d65ba787172a6d8b788d45b9a65def2e7ba5b79a
Thomas Kristiansen <thom_lfc@hotmail.com>
2014-11-04 02:29:47 +0100

    Oppdatert spillet med noen få endringer

--- Game 1.0/script.js
+++ Game 1.0/script.js
@@ -1,36 +1,47 @@
-var timer;
+var timer1;
+var timer2;
 var flyttTimer;
-var tall = 0;
 var points = 0;
 var forrigeknapp = true;
+var knapp = 0;
 
 function flyttFigur(event) {
-	if(forrigeknapp) {
-		switch(event.keyCode) {
-		case 37:	flyttTimer = window.setInterval(venstre, 0.1);
+	switch(event.keyCode) {
+	case 37:	if(forrigeknapp || knapp == 0) {
+					clearInterval(flyttTimer);
+					flyttTimer = window.setInterval(venstre, 0.1);
 					forrigeknapp = false;
-					break;
-		case 39:	flyttTimer = window.setInterval(hoyre, 0.1);
+					knapp = 1;
+				}
+				break;
+	case 39:	if(forrigeknapp || knapp == 1) {
+					clearInterval(flyttTimer);
+					flyttTimer = window.setInterval(hoyre, 0.1);
 					forrigeknapp = false;
-					break;
-		default:
-		}
+					knapp = 0;
+				}
+				break;
+	default:
 	}
 }
 
 function hoyre() {
 	var element = document.getElementById("figur");
-	var x = element.offsetLeft;
-	element.style.left = (x + 2) + "px";
+	for(var i = 0; i < 5; i++) {
+		var x = element.offsetLeft;
+		element.style.left = (x + 1) + "px";
+	}
 		
 	sjekkPos();
 }
 
 function venstre() {
 	var element = document.getElementById("figur");
-	var x = element.offsetLeft;
-	element.style.left =  (x - 2) + "px";
-		
+	for(var i = 0; i < 5; i++) {
+		var x = element.offsetLeft;
+		element.style.left =  (x - 1) + "px";
+	}
+	
 	sjekkPos();
 }
 
@@ -59,31 +70,42 @@ function sjekkPos() {
 }
 
 function start() {
-	tall = -20;
-	points = 0;
 	document.getElementById("start").disabled = true;
 	document.getElementById("dot1").style.left = Math.floor(Math.random() * 790) + "px";
-	timer = window.setInterval(fall, 100);
+	window.setTimeout(lagdiv, 1000);
+	timer1 = window.setInterval(function() {fall("dot1")}, 100);
 }
 
-function fall() {
-	tall += 20;
-	var element = document.getElementById("dot1");
+function lagdiv() {
+	var div = document.createElement("div");
+	div.setAttribute("class", "dot");
+	div.setAttribute("id", "dot2");
+	document.getElementById("ramme").appendChild(div);
+	timer2 = window.setInterval(function() {fall("dot2")}, 100);
+}
+
+function fall(id) {
+	var element = document.getElementById(id);
 	var elementF = document.getElementById("figur");
+	var tall = element.offsetTop;
+	tall += 20;
 	var txt = "";
 	element.style.top = tall + "px";
-	if(element.offsetTop == 400) {
+	if(element.offsetTop >= 400) {
 		var maxL = elementF.offsetLeft;
 		var maxR = maxL + elementF.offsetWidth;
 		var dotL = element.offsetLeft;
 		var dotR = dotL + element.offsetWidth;
 		if(dotR > maxL && dotL < maxR) {
 			points += 10;
-			tall = -20;
+			element.style.top = "-20px";
 			element.style.left = Math.floor(Math.random() * 790) + "px";
 		}
 		else {
-			clearInterval(timer);
+			clearInterval(timer1);
+			clearInterval(timer2);
+			document.getElementById("dot2").style.top = "-20px";
+			document.getElementById("dot1").style.top = "-20px";
 			txt = "GAME OVER. Din score: ";
 			document.getElementById("start").disabled = false;
 			document.getElementById("start").innerHTML = "Prøv igjen!";
