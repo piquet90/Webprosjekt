14c0887da8a9b2e31a00cb05b1af7d9ff98e9e12
Rudi Wei Yu <rudiwyu@gmail.com>
2014-11-06 14:50:32 +0100

    endret litt css

---
+++ controller/statsController.php
@@ -0,0 +1,21 @@
+<?php
+
+Class statsController Extends baseController {
+
+
+
+	public function index() 
+	{
+		/*
+		$query = "SELECT * FROM userxp WHERE uid='$uid'";
+		$result = queryMysql($query);
+
+		$row = $result->fetch_array(MYSQLI_ASSOC);
+		*/
+
+		$this->registry->template->show('hardware');
+	}
+
+}
+
+?>
