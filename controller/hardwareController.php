fdbde7d1b2a3b00951ff78c4a97258a017b6c741
Rudi Wei Yu <rudiwyu@gmail.com>
2014-11-08 23:08:24 +0100

    hardwarestore fixes

--- controller/hardwareController.php
+++ controller/hardwareController.php
@@ -6,19 +6,19 @@ Class hardwareController Extends baseController {
 
 	public function index() 
 	{
-		/*
-		$query = "SELECT * FROM userxp WHERE uid='$uid'";
+		$uid = $_SESSION['uid'];
+		$query = "SELECT * FROM users WHERE uid='$uid'";
 		$result = queryMysql($query);
-
-		$row = $result->fetch_array(MYSQLI_ASSOC);
-		*/
-		
-		$this->registry->template->hw1 = 'Du eier denne" disabled';
-		$this->registry->template->hw2 = 'Oppgrader!"';
-		$this->registry->template->hw3 = 'Du eier denne" disabled';
-		$this->registry->template->hw4 = 'Du eier denne" disabled';
-		$this->registry->template->hw5 = 'Du eier denne" disabled';
-
+		$row = $result->fetch_assoc();
+		$hwlevel = $row['hwlevel'];
+		$this->registry->template->formpath = makePath('hardware/upgrade');
+		for($i=1; $i<=5; $i++)
+		{
+			$this->registry->template->{"hwl".$i} = $i;
+			if($hwlevel > $i) $this->registry->template->{"hw".$i} = 'Nedgradering" disabled';
+			else if($hwlevel == $i) $this->registry->template->{"hw".$i} = 'Du eier denne" disabled';
+			else if($hwlevel < $i) $this->registry->template->{"hw".$i} = 'Kjøp"';
+		}
 		$this->registry->template->show('hardware');
 	}
 
--- views/hardware.php
+++ views/hardware.php
@@ -5,35 +5,55 @@ echo <<<_END
 <h3>Hardware Store</h3>
 
 <div class="product">
-<h4>DELL LATITUDE<span> - $100</span></h4>
+<h4>IBM 4Kilo</h4>
 <img src="img/computer1.jpg" />
-<p>Dell latitude laptop. Den suger balle.</p>
+<p>This laptop weighs 4 Kilos. Looks like something found in your moms basement.</p>
+<form action="$formpath" method="post"> 
+<input type="hidden" name="hwlvl" value="$hwl1" />
 <input type="submit" value="$hw1>
+</form>
 </div>
+
 <div class="product">
-<h4>DELL LATITUDE</h4>
+<h4>IBM 4Kilo</h4>
 <img src="img/computer1.jpg" />
-<p>Dell latitude laptop. Den suger balle.</p>
+<p>This laptop weighs 4 Kilos. Looks like something found in your moms basement.</p>
+<form action="$formpath" method="post"> 
+<input type="hidden" name="hwlvl" value="$hwl2" />
 <input type="submit" value="$hw2>
+</form>
 </div>
+
 <div class="product">
-<h4>DELL LATITUDE</h4>
+<h4>IBM 4Kilo</h4>
 <img src="img/computer1.jpg" />
-<p>Dell latitude laptop. Den suger balle.</p>
+<p>This laptop weighs 4 Kilos. Looks like something found in your moms basement.</p>
+<form action="$formpath" method="post"> 
+<input type="hidden" name="hwlvl" value="$hwl3" />
 <input type="submit" value="$hw3>
+</form>
 </div>
+
 <div class="product">
-<h4>DELL LATITUDE</h4>
+<h4>IBM 4Kilo</h4>
 <img src="img/computer1.jpg" />
-<p>Dell latitude laptop. Den suger balle.</p>
+<p>This laptop weighs 4 Kilos. Looks like something found in your moms basement.</p>
+<form action="$formpath" method="post"> 
+<input type="hidden" name="hwlvl" value="$hwl4" />
 <input type="submit" value="$hw4>
+</form>
 </div>
+
 <div class="product">
-<h4>DELL LATITUDE</h4>
+<h4>IBM 4Kilo</h4>
 <img src="img/computer1.jpg" />
-<p>Dell latitude laptop. Den suger balle.</p>
+<p>This laptop weighs 4 Kilos. Looks like something found in your moms basement.</p>
+<form action="$formpath" method="post"> 
+<input type="hidden" name="hwlvl" value="$hwl5" />
 <input type="submit" value="$hw5>
+</form>
 </div>
+
 
 
 </div>
