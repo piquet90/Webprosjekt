a70769ee3e630a8877fe2ba2da2c2ad4026dee9d
Rudi Wei Yu <rudiwyu@gmail.com>
2014-11-09 00:23:53 +0100

    la til forskjellige beskrivelser av hardware og bilder

--- views/hardware.php
+++ views/hardware.php
@@ -2,52 +2,53 @@
 
 echo <<<_END
 <div id="main" class="scroll">
-<h3>Hardware Store</h3>
 
 <div class="product">
 <h4>IBM 4Kilo</h4>
 <img src="img/computer1.jpg" />
-<p>This laptop weighs 4 Kilos. Looks like something found in your moms basement.</p>
+<p>Laptop from your moms basement. 5% reduced cooldown on jobs.</p>
+</div>
+
+<div class="product">
+<h4>IBM 4Kilo</h4>
+<img src="img/computer1.jpg" />
+<p>Laptop from your moms basement. 5% reduced cooldown on jobs.</p>
 <form action="$formpath" method="post"> 
 <input type="hidden" name="hwlvl" value="$hwl1" />
 <input type="submit" value="$hw1>
 </form>
 </div>
-
 <div class="product">
-<h4>IBM 4Kilo</h4>
-<img src="img/computer1.jpg" />
-<p>This laptop weighs 4 Kilos. Looks like something found in your moms basement.</p>
+<h4>Asia LATOP FLEXI</h4>
+<img src="img/computer2.jpg" />
+<p>Asian creation of sensation. 10% reduced cooldown on jobs.</p>
 <form action="$formpath" method="post"> 
 <input type="hidden" name="hwlvl" value="$hwl2" />
 <input type="submit" value="$hw2>
 </form>
 </div>
-
 <div class="product">
-<h4>IBM 4Kilo</h4>
-<img src="img/computer1.jpg" />
-<p>This laptop weighs 4 Kilos. Looks like something found in your moms basement.</p>
+<h4>Dell Latitude</h4>
+<img src="img/computer3.jpg" />
+<p>A really average laptop. 25% reduced cooldown on jobs.</p>
 <form action="$formpath" method="post"> 
 <input type="hidden" name="hwlvl" value="$hwl3" />
 <input type="submit" value="$hw3>
 </form>
 </div>
-
 <div class="product">
-<h4>IBM 4Kilo</h4>
-<img src="img/computer1.jpg" />
-<p>This laptop weighs 4 Kilos. Looks like something found in your moms basement.</p>
+<h4>Lenovo Thinkpad</h4>
+<img src="img/computer4.jpg" />
+<p>The classic Lenovo Thinkpad. 30% reduced cooldown on jobs.</p>
 <form action="$formpath" method="post"> 
 <input type="hidden" name="hwlvl" value="$hwl4" />
 <input type="submit" value="$hw4>
 </form>
 </div>
-
 <div class="product">
-<h4>IBM 4Kilo</h4>
-<img src="img/computer1.jpg" />
-<p>This laptop weighs 4 Kilos. Looks like something found in your moms basement.</p>
+<h4>Macbook Pro</h4>
+<img src="img/computer5.jpg" />
+<p>Top specced Macbook Pro. Your a real player. 50% reduced cooldown on jobs.</p>
 <form action="$formpath" method="post"> 
 <input type="hidden" name="hwlvl" value="$hwl5" />
 <input type="submit" value="$hw5>
