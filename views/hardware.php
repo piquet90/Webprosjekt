60f46a64c06fc7dc7bf8373956b611a18da9d123
audunb <audun@hbru.no>
2014-11-10 20:39:00 +0100

    Fiksa hardware-siden og diverse

--- views/hardware.php
+++ views/hardware.php
@@ -3,57 +3,103 @@
 echo <<<_END
 <div id="main" class="scroll">
 
-<div class="product">
-<h4>IBM 4Kilo</h4>
-<img src="img/computer1.jpg" />
-<p>Laptop from your moms basement. 5% reduced cooldown on jobs.</p>
-</div>
+<h3>HARDWARE</h3>
 
 <div class="product">
 <h4>IBM 4Kilo</h4>
+
 <img src="img/computer1.jpg" />
-<p>Laptop from your moms basement. 5% reduced cooldown on jobs.</p>
-<form action="$formpath" method="post"> 
-<input type="hidden" name="hwlvl" value="$hwl1" />
-<input type="submit" value="$hw1>
-</form>
+
+<p>Laptop from your moms basement. No cooldown on jobs</p>
+<input type="submit" value="$hw1" disabled>
 </div>
+
 <div class="product">
-<h4>Asia LATOP FLEXI</h4>
+<h4>Asia LAPTOP FLEXI</h4>
+
 <img src="img/computer2.jpg" />
+
 <p>Asian creation of sensation. 10% reduced cooldown on jobs.</p>
 <form action="$formpath" method="post"> 
 <input type="hidden" name="hwlvl" value="$hwl2" />
-<input type="submit" value="$hw2>
+<input type="submit" value="$hw2">
 </form>
 </div>
+
+<div class="product">
+<h4>Amsung</h4>
+
+
+<p>The great amsung. Due to discontinuation of Amsung laptops, this option is no longer available</p>
+<form action="$formpath" method="post"> 
+<input type="hidden" name="hwlvl" value="$hwl1" />
+
+</form>
+</div>
+
 <div class="product">
 <h4>Dell Latitude</h4>
+
 <img src="img/computer3.jpg" />
+
 <p>A really average laptop. 25% reduced cooldown on jobs.</p>
 <form action="$formpath" method="post"> 
 <input type="hidden" name="hwlvl" value="$hwl3" />
-<input type="submit" value="$hw3>
+<input type="submit" value="$hw3">
 </form>
 </div>
+
 <div class="product">
 <h4>Lenovo Thinkpad</h4>
+
 <img src="img/computer4.jpg" />
+
 <p>The classic Lenovo Thinkpad. 30% reduced cooldown on jobs.</p>
 <form action="$formpath" method="post"> 
 <input type="hidden" name="hwlvl" value="$hwl4" />
-<input type="submit" value="$hw4>
+<input type="submit" value="$hw4">
 </form>
 </div>
+
+
 <div class="product">
-<h4>Macbook Pro</h4>
-<img src="img/computer5.jpg" />
-<p>Top specced Macbook Pro. Your a real player. 50% reduced cooldown on jobs.</p>
+<h4>Something</h4>
+
+
+
+<p>40% reduced cooldown on jobs.</p>
 <form action="$formpath" method="post"> 
 <input type="hidden" name="hwlvl" value="$hwl5" />
-<input type="submit" value="$hw5>
+<input type="submit" value="$hw5">
 </form>
 </div>
+
+
+<div class="product">
+<h4>Macbook Pro</h4>
+
+<img src="img/computer5.jpg" />
+
+<p>Top specced Macbook Pro. Money well spent! Not. 50% reduced cooldown on jobs.</p>
+<form action="$formpath" method="post"> 
+<input type="hidden" name="hwlvl" value="$hwl5" />
+<input type="submit" value="$hw5">
+</form>
+</div>
+
+<div class="product">
+<h4>MLG</h4>
+
+<img src="img/computerMLG.jpg" />
+
+<p>bro you sik. 60% cooldown?</p>
+<form action="$formpath" method="post"> 
+<input type="hidden" name="hwlvl" value="$hwl5" />
+<input type="submit" value="$hw5">
+</form>
+</div>
+
+
 
 
 
