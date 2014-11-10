8b5dd3dba6e6af0b99728227d6338af5763ac882
Thomas Kristiansen <thom_lfc@hotmail.com>
2014-11-10 21:37:39 +0100

    fiksa at tidsformat på counter er ##:##:## og endra en liten bug på jobblink når cooldown er ferdig

--- views/jobs.php
+++ views/jobs.php
@@ -5,24 +5,28 @@ echo <<<_END
 var sekunder = [];
 var interval = [];
 
-function tell(navn) {
+function tell(navn, plass) {
 	var sek = sekunder[navn]
 	sek = document.getElementById(navn).innerHTML;
 	sekunder[navn] = parseInt(sek, 10);
-	update(navn);
-	interval[navn] = window.setInterval(function() {update(navn);}, 1000);
+	update(navn, plass);
+	interval[navn] = window.setInterval(function() {update(navn, plass);}, 1000);
 }
 
-function update(id) {
+function update(id, plass) {
 	sekunder[id]--;
 	var sek = sekunder[id];
 	var timer = sek / 3600;
 	var min = (sek % 3600) / 60;
 	var restSek = (sek % 3600) % 60;
-	document.getElementById(id).innerHTML = Math.floor(timer) + ":" + Math.floor(min) + ":" + Math.floor(restSek);
+	timer = Math.floor(timer);
+	min = Math.floor(min);
+	restSek = Math.floor(restSek);
+	document.getElementById(id).innerHTML = ("0" + timer).slice(-2) + ":" + ("0" + min).slice(-2) + ":" + ("0" + restSek).slice(-2);
 	if(sek <= 0) {
-		clearInterval(interval[id]	);
-		document.getElementById(id).innerHTML = '<a href="index.php?rt=jobs/simple">Simple Job</a>';
+		clearInterval(interval[id]);
+		document.getElementById(id).innerHTML = "";
+		document.getElementById(plass).innerHTML = '<a href="index.php?rt=jobs/simple">Simple Job</a>';
 	}
 }
 </script>
@@ -31,8 +35,8 @@ function update(id) {
 <p>Her kommer det jobber som man kan gjøre</p>
 
 <ol>
-<li>$simple</li>
-<li><div id="teller2">26000</div><script>tell("teller2");</script></li>
+<li id="plass1">$simple</li>
+<li id="plass2"><div id="teller2">26000</div><script>tell("teller2", "plass2");</script></li>
 <li></li>
 <li></li>
 </ol>
