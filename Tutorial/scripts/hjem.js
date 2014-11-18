08bfa93614aadbcf0b4bf5ebdf92fd2bf624a68f
Thomas Kristiansen <thom_lfc@hotmail.com>
2014-11-18 21:41:35 +0100

    fiksa grå box uansett zoom og oppløsning

--- Tutorial/scripts/hjem.js
+++ Tutorial/scripts/hjem.js
@@ -1,3 +1,23 @@
-function () {
-
+function load() {
+	var wrapper = document.getElementById("wrapper");
+	var box = document.getElementById("grayBox");
+	var wrapW = wrapper.offsetWidth;
+	var wrapH = wrapper.offsetHeight;
+	var wrapL = wrapper.offsetLeft;
+	var wrapT = wrapper.offsetTop;
+	var windowH = window.innerHeight;
+	
+	var width = (wrapL * 2) + wrapW + "px";
+	var height;
+	if((wrapH + wrapT) <= windowH) {
+		height = windowH + "px";
+	}
+	else {
+		height = wrapH + wrapT + 10 + "px";
+	}
+	
+	if(box.offsetHeight != height && box.offsetWidth != width) {
+		box.style.height = height;
+		box.style.width = width;
+	}
 }
