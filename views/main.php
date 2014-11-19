3bc8c70e1a9e36d89d7559b16675e4fcf712be7f
Rudi Wei Yu <rudiwyu@gmail.com>
2014-11-19 16:48:17 +0100

    fikset små bugs og endel stats

--- views/main.php
+++ views/main.php
@@ -5,6 +5,9 @@ echo <<<_END
 <h3>Welcome</h3>
 <p>Welcome to Silicon Valley Startup. We are currently working on
 improving this website. More functions can come at any time.</p>
+<h4>19. November</h4>
+<p>User testing has started. Be aware that alot of functions are 
+still under construction and description might not be accurate</p>
 </div>
 
 _END;
