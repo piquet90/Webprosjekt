5a21549f63f928244d5ab6ae9d48dd4b2d57fac6
Rudi Wei Yu <rudiwyu@gmail.com>
2014-11-19 14:09:21 +0100

    Fikset litt stats. Skal nå vise mest cash og XP.

--- controller/statsController.php
+++ controller/statsController.php
@@ -6,28 +6,42 @@ Class statsController Extends baseController {
 
 	public function index() 
 	{
-		$query = "SELECT * FROM users";
+		$query = "SELECT username FROM users ORDER BY uid DESC LIMIT 5";
 
 		$result = queryMysql($query);
-		$users = "";
+		$newestUsers = "";
 		$number = $result->num_rows;
 
 		while($row = $result->fetch_assoc()) 
 		{
-        	$users .= ucfirst($row['username'])."<br />";
+        	$newestUsers .= ucfirst($row['username'])."<br />";
+   		}
+
+   		$query = "SELECT username, saldo FROM users ORDER BY saldo DESC LIMIT 5";
+   		$richest = "";
+   		$result = queryMysql($query);
+   		while($row = $result->fetch_assoc())
+   		{
+   			$richest .= ucfirst($row['username'])." $".$row['saldo']."<br />";
+   		}
+
+   		$query = "SELECT username, xp FROM users ORDER BY saldo DESC LIMIT 5";
+   		$mostxp = "";
+   		$result = queryMysql($query);
+   		while($row = $result->fetch_assoc())
+   		{
+   			$mostxp .= ucfirst($row['username'])." ".$row['xp']."<br />";
    		}
 
 
 
+
+   		// display variables
+   		$this->registry->template->mostxp = $mostxp;
+   		$this->registry->template->richestUsers = $richest;
    		$this->registry->template->usernumber = $number;
-   		$this->registry->template->users = $users;
-
-
-
-
-
-
-
+   		$this->registry->template->newestUsers = $newestUsers;
+   		// gogo display
 		$this->registry->template->show('stats');
 	}
 
