e028ba04941729730d621477bd8af44717d0d8a1
Thomas Kristiansen <thom_lfc@hotmail.com>
2014-11-24 10:07:47 +0100

    fiksa tekst og sånt på spillet

--- views/game1.php
+++ views/game1.php
@@ -2,9 +2,11 @@
 
 echo <<<_END
 <div id="main" class="scroll">
-<canvas id="ramme">Your browser does not support the HTML5 canvas tag.</canvas>
-<div id="knapp" onclick="start()">Start!</div>
-<script src="scripts/game1.js" type="text/javascript"></script>
+<div id="spillWrapper">
+	<canvas id="ramme">Your browser does not support the HTML5 canvas tag.</canvas>
+	<div id="knapp" onclick="start()">Start!</div>
+	<script src="scripts/game1.js" type="text/javascript"></script>
+</div>
 </div>
 
 _END;
