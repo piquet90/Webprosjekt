57af949fcc5739ce06d37ac9a80364a57c0394a8
Elnan <olavelnan@gmail.com>
2014-11-26 18:10:09 +0100

    Goalscorer! og brodtekst

--- views/minigame.php
+++ views/minigame.php
@@ -6,10 +6,10 @@ echo <<<_END
 <div class="game">
 
 <div class="gamewrapper">
-<h4>fobbalspillet</h4>
-<img src="img/fotballspillet.jpg" alt="Skjermdump av fotballspillet" />
+<h4>Goalscorer!</h4>
+<img src="img/fotballspillet.jpg" alt="Skjermdump av fotballspillet hvor man ser en gressmatte i bakgrunnen og et fotballmål i bunnen av bildeskjermen" />
 
-<p>Prøv ut hvordan det er å være en fotballkeeper! Få alle ballene i målet, som du styrer med piltastene. Spillet er laget i JavaScript.</p>
+<p>I dette spillet styrer du et fotballmål som må samle alle de fallende fotballene i nettet. Du styrer med piltastene. <p>Spillet er laget i JavaScript.</p></p>
 </div>
 
 <a href="$game1"><div class="run">Trykk her for å spille</div></a>
