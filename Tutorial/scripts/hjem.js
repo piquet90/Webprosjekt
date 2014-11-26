2fff611f7645c4d159ff02fe6d15e0ee3b1d9c76
Thomas Kristiansen <thom_lfc@hotmail.com>
2014-11-26 03:46:20 +0100

    fiksa mer på tutorial

--- Tutorial/scripts/hjem.js
+++ Tutorial/scripts/hjem.js
@@ -2,37 +2,24 @@ function start() {
 	setInterval(boxSize, 1);
 	
 	var text = "Dette er spillets hovedside. Dette er hovedsentralen for hele spillet!"
-				+ "<br/><br/><div onclick='navBar()' class='link'>Neste</div>";
+				+ "<br /><br /><div onclick='navBar()' class='link'>Neste</div>";
 	
 	document.getElementById("textBox").innerHTML = text;
 }
 
 function boxSize() {
-	var wrapper = document.getElementById("wrapper");
-	var box = document.getElementById("grayBox");
-	var wrapW = wrapper.offsetWidth;
-	var wrapH = wrapper.offsetHeight;
-	var wrapL = wrapper.offsetLeft;
-	var wrapT = wrapper.offsetTop;
-	var windowH = window.innerHeight;
+	var gray = document.getElementById("grayBox");
+	gray.style.height = "0";
+	gray.style.height = document.body.scrollHeight + "px";
 	
-	var width = (wrapL * 2) + wrapW;
-	var height;
-	if((wrapH + wrapT) <= windowH) {
-		height = windowH + 10;
-	}
-	else {
-		height = wrapH + wrapT + 10;
-	}
-	width --;
-	box.style.height = height + "px";
-	box.style.width = width + "px";
+	gray.style.width = "0";
+	gray.style.width = document.body.scrollWidth + "px";
 }
 
 function navBar() {
 	document.getElementById("navbar").style.zIndex = "10";
 	var text = "Dette er navigasjonsbaren din. Her står all info om deg."
-				+ "<br/><br/><div onclick='topBar()' class='link'>Neste</div>";
+				+ "<br /><br /><div onclick='topBar()' class='link'>Neste</div>";
 	document.getElementById("textBox").innerHTML = text;
 }
 
@@ -40,6 +27,6 @@ function topBar() {
 	document.getElementById("navbar").style.zIndex = "1";
 	document.getElementById("topbar").style.zIndex = "10";
 	var text = "Dette er hovedmenyen. Det er her du navigerer deg gjennom alle ting du kan gjøre i spillet."
-				+ "<br/><br/>La oss trykke på Jobber og sjekke ut denne funksjonen!";
+				+ "<br /><br />La oss trykke på Jobber og sjekke ut denne funksjonen!";
 	document.getElementById("textBox").innerHTML = text;
 }
